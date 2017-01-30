97b84f96e41c715b844d005f4a0dc1a98e2bbfca
Guanqun Mao <guanqunm@gmail.com>
2017-01-30 10:57:47 -0800

    Updated code and pics

--- hw1/src/Q2.py
+++ hw1/src/Q2.py
@@ -1,67 +1,253 @@
-import matplotlib.pyplot as plt
+from math import sqrt
+import numpy as np
 import pandas as pa
-from sklearn import linear_model, cross_validation
-import numpy
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.cross_validation import KFold
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import f_regression
+
+from pybrain import *
+from pybrain.datasets import SupervisedDataSet
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
 
 
-# Get the rows from the 'selected' index.
-def get_selected(all_elem, selected):
-    result = []
-    for i in selected:
-        result.append(all_elem[i])
-    return result
+# Binary the data that has no numerical meaning
+def preprocessed_1(network):
+    target_forgot = network.values[:, 6:7]
+    rowNO = len(network)
+    ##transform the origin date into the proper forms
+    ##the final result is in prepossessed
+    ##meaning of its columns
+    ##0:15 week #
+    ##15:22 weekdays
+    ##22:28 startTime
+    ##28:33 workflow
+    ##33:63 fileName
+    ##63:64 Backup Time(hour)
+    weeks = network.values[:, 0:1]
+    bweeks = np.zeros((rowNO, 15))
+    i = 0
+    for tmp in np.nditer(weeks):
+        bweeks[i, int(tmp) - 1] = 1
+        i += 1
+
+    weekdays = network.values[:, 1:2]
+    bweekdays = np.zeros((rowNO, 7))
+    i = 0
+    for tmp in np.nditer(weekdays):
+        bweekdays[i, int(tmp) - 1] = 1
+        i += 1
+
+    startTimes = network.values[:, 2:3]
+    bstartTimes = np.zeros((rowNO, 6))
+    i = 0
+    for tmp in np.nditer(startTimes):
+        bstartTimes[i, int(tmp) / 4] = 1
+        i += 1
+
+    workflows = network.values[:, 3:4]
+    bworkflows = np.zeros((rowNO, 5))
+    i = 0
+    for tmp in np.nditer(workflows):
+        bworkflows[i, int(tmp)] = 1
+        i += 1
+
+    fileNames = network.values[:, 4:5]
+    bfileNames = np.zeros((rowNO, 30))
+    i = 0
+    for tmp in np.nditer(fileNames):
+        bfileNames[i, int(tmp)] = 1
+        i += 1
+
+    preprocessed = np.concatenate((bweeks, bweekdays, bstartTimes, bworkflows, bfileNames, target_forgot), axis=1)
+    return preprocessed
 
 
-def Q2A(network):
-    network_X = network.values[:, (0, 1, 2, 3, 4, 6)]
-    network_Y = network.values[:, 5]
-    model = linear_model.LinearRegression()
-
-    regr = linear_model.LinearRegression()
-    kf = cross_validation.KFold(len(network_X), 10, True)
+# Compare the linear regression model and random forest regression model
+def linear_and_random_forest_regression(data, target, network):
+    lr = linear_model.LinearRegression(normalize=True)
+    rfr = RandomForestRegressor(n_estimators=30, max_depth=12, max_features='auto')
+    kf = KFold(len(target), n_folds=10, shuffle=True, random_state=None)
+    RMSE_LINEAR = []
+    RMSE_RFR = []
     for train_index, test_index in kf:
-        network_X_train = get_selected(network_X, train_index)
-        network_Y_train = get_selected(network_Y, train_index)
-        regr.fit(network_X_train, network_Y_train)
+        data_train, data_test = data[train_index], data[test_index]
+        target_train, target_test = target[train_index], target[test_index]
+        lr.fit(data_train, target_train)
+        rfr = rfr.fit(data_train, target_train)
+        rmse_linear = sqrt(np.mean((lr.predict(data_test) - target_test) ** 2))
+        RMSE_LINEAR.append(rmse_linear)
+        rmse_rfr = sqrt(np.mean((rfr.predict(data_test) - target_test) ** 2))
+        RMSE_RFR.append(rmse_rfr)
 
-    predicted = cross_validation.cross_val_predict(model, network_X, network_Y, 10, 1, 0, None, 0)
-    scores = cross_validation.cross_val_score(model, network_X, network_Y, cv=10, scoring='mean_squared_error')
+    # scores = cross_validation.cross_val_score(rfr,data_test, target_test.ravel, cv=10)
+    # print np.mean(scores)
 
-    print 'All RMSEs', numpy.sqrt(-scores)
-    print 'Mean RMSE', numpy.mean(numpy.sqrt(-scores))
-    print 'Best RMSE', numpy.min(numpy.sqrt(-scores))
-    print 'Coefficients', regr.coef_
+    F, pval = f_regression(data_test, lr.predict(data_test))
+    #print(np.mean(RMSE_RFR))
+    print(pval)
+    test_times = np.arange(1, 11)
+    plt.figure()
+    plt.plot(test_times, RMSE_LINEAR, label="RMSE in linear regression with 10-fold cv")
+    plt.plot(test_times, RMSE_RFR, label="RMSE in random forest regression with 10-fold cv")
+    plt.ylim(0.0, 0.12)
+    plt.title("linear regression and random forest regression RMSE")
+    plt.xlabel("cross validation #")
+    plt.ylabel("RMSE")
+    plt.legend()
 
-    # Residual
-    residual = []
-    for i in range(len(network_X)):
-        residual.append(network_Y[i] - predicted[i])
+    network['predicted_lr'] = lr.predict(data)
+    network['predicted_rfr'] = rfr.predict(data)
+    network_time_target = network.groupby(["Week #", "Day of Week", "Backup Start Time - Hour of Day"])[
+        "Size of Backup (GB)"].sum()
+    network_time_predict_lr = network.groupby(["Week #", "Day of Week", "Backup Start Time - Hour of Day"])[
+        "predicted_lr"].sum()
+    network_time_predict_rfr = network.groupby(["Week #", "Day of Week", "Backup Start Time - Hour of Day"])[
+        "predicted_rfr"].sum()
+    time = np.arange(1, len(network_time_target) + 1)
 
-    # Plot outputs
-    plt.scatter(range(len(network_X)), network_Y, color='black')
-    plt.scatter(range(len(network_X)), predicted, color='blue')
-    # plt.scatter(residual, predicted, color='red')
+    plt.figure()
+    plt.scatter(time, network_time_target, s=15, color='red', label="Actual values over time")
+    plt.scatter(time, network_time_predict_lr, s=15, color='green', label="predicted values with linear model")
+    plt.xlabel('Time')
+    plt.ylabel('Size of backup(GB)')
+    plt.ylim(-2, 12)
+    plt.legend()
 
+    plt.figure()
+    plt.plot(time[0:120], network_time_predict_rfr[0:120], label="predicted values with random forest tree model")
+    plt.legend()
+
+    plt.figure()
+    plt.scatter(lr.predict(data), lr.predict(data) - target, label="residual VS fitted values")
+    plt.xlabel("fitted values")
+    plt.ylabel("residual")
+    plt.legend()
+    plt.ylim(-0.8, 0.4)
     plt.show()
+    return RMSE_LINEAR
+
+
+# Tuning the parameter of randomforest model.
+def random_forest_tuning_parameters(data, target, network):
+    kf = KFold(len(target), 10, shuffle=True);
+    RMSE_BEST = 10
+    rfr_best = RandomForestRegressor(n_estimators=30, max_features=len(data[0]), max_depth=8)
+    for nEstimators in range(29, 31, 1):
+        for maxFeatures in range(len(data[0]) - 1, len(data[0] + 1)):
+            for maxDepth in range(11, 13, 1):
+                rfr = RandomForestRegressor(n_estimators=nEstimators, max_features=maxFeatures, max_depth=maxDepth)
+                RMSE_RFR = []
+                for train_index, test_index in kf:
+                    data_train, data_test = data[train_index], data[test_index]
+                    target_train, target_test = target[train_index], target[test_index]
+                    rfr.fit(data_train, target_train)
+                    rmse_rfr = sqrt(np.mean((rfr.predict(data_test) - target_test) ** 2))
+                    RMSE_RFR.append(rmse_rfr)
+                if RMSE_BEST > np.mean(RMSE_RFR):
+                    rfr_best = rfr
+                    RMSE_BEST = np.mean(RMSE_RFR)
+    kf_final = KFold(len(target), 10, shuffle=True);
+    RMSE_FINAL = []
+    for train_index, test_index in kf_final:
+        data_train, data_test = data[train_index], data[test_index]
+        target_train, target_test = target[train_index], target[test_index]
+        rfr_best.fit(data_train, target_train)
+        rmse_rfr = sqrt(np.mean((rfr_best.predict(data_test) - target_test) ** 2))
+        RMSE_FINAL.append(rmse_rfr)
+    plt.figure()
+    plt.plot(range(1, len(RMSE_FINAL) + 1), RMSE_FINAL)
+    plt.title("The best RMSE with random forest")
+    plt.xlabel("cross validation times")
+    plt.ylabel("RMSE")
+    plt.show()
+    print(np.mean(RMSE_FINAL))
+    return RMSE_FINAL
+
+
+# Fit the data with neural network
+def neural_network(data, target, network):
+    DS = SupervisedDataSet(len(data[0]), 1)
+    nn = buildNetwork(len(data[0]), 7, 1, bias=True)
+    kf = KFold(len(target), 10, shuffle=True);
+    RMSE_NN = []
+    for train_index, test_index in kf:
+        data_train, data_test = data[train_index], data[test_index]
+        target_train, target_test = target[train_index], target[test_index]
+        for d, t in zip(data_train, target_train):
+            DS.addSample(d, t)
+        bpTrain = BackpropTrainer(nn, DS, verbose=True)
+        # bpTrain.train()
+        bpTrain.trainUntilConvergence(maxEpochs=10)
+        p = []
+        for d_test in data_test:
+            p.append(nn.activate(d_test))
+
+        rmse_nn = sqrt(np.mean((p - target_test) ** 2))
+        RMSE_NN.append(rmse_nn)
+        DS.clear()
+    time = range(1, 11)
+    plt.figure()
+    plt.plot(time, RMSE_NN)
+    plt.xlabel('cross-validation time')
+    plt.ylabel('RMSE')
+    plt.show()
+    print(np.mean(RMSE_NN))
+
+
+def neural_network_converg(data, target, network):
+    DS = SupervisedDataSet(len(data[0]), 1)
+    nn = buildNetwork(len(data[0]), 7, 1, bias=True, hiddenclass=SigmoidLayer, outclass=LinearLayer)
+    for d, t in zip(data, target):
+        DS.addSample(d, t)
+    Train, Test = DS.splitWithProportion(0.9)
+    # data_train = Train['input']
+    data_test = Test['input']
+    # target_train = Train['target']
+    target_test = Test['target']
+    bpTrain = BackpropTrainer(nn, Train, verbose=True)
+    # bpTrain.train()
+    bpTrain.trainUntilConvergence(maxEpochs=10)
+    p = []
+    for d_test in data_test:
+        p.append(nn.activate(d_test))
+
+    rmse_nn = sqrt(np.mean((p - target_test) ** 2))
+    print(rmse_nn)
 
 
 def main():
     network = pa.read_csv("../Data/network_backup_dataset.csv", header=0)
     dict = {"Monday": "1", "Tuesday": "2", "Wednesday": "3", "Thursday": "4", "Friday": "5", "Saturday": "6",
             "Sunday": "7"}
-    workflows = pa.unique(network["Work-Flow-ID"])
-    colors = {workflows[0]: "red", workflows[1]: "orange", workflows[2]: "yellow", workflows[3]: "green",
-              workflows[4]: "blue"}
-
     for i in dict:
-        network["Day of Week"] = [s.replace(i, dict[i]) for s in network["Day of Week"]]
+        network['Day of Week'] = [s.replace(i, dict[i]) for s in network['Day of Week']]
 
     network['File Name'] = [s.replace("File_", "") for s in network['File Name']]
     network['Work-Flow-ID'] = [s.replace("work_flow_", "") for s in network['Work-Flow-ID']]
-    network['Day of Week'] = [int(s) for s in network['Day of Week']]
+    network['Day of Week'] = [int(s) - 1 for s in network['Day of Week']]
     network['File Name'] = [int(s) for s in network['File Name']]
     network['Work-Flow-ID'] = [int(s) for s in network['Work-Flow-ID']]
-    Q2A(network)
+
+    data1 = network.values[:, 0:5]
+    data2 = network.values[:, 6:7]
+    data = np.concatenate((data1, data2), axis=1)
+    target = network.values[:, 5]
+    ### if you want to make the data into binary format, uncomment the line below
+    #data = preprocessed_1(network)
+
+    ### compare the linear model with the random forest model
+    #linear_and_random_forest_regression(data, target, network)
+
+
+    ### Tuning the parameters of the random forest
+    #random_forest_tuning_parameters(data, target, network)
+
+
+    ### use the neural network model to fit the data
+    #neural_network(data, target, network)
 
 
 if __name__ == "__main__":
---
+++ hw1/src/Q4.py
@@ -0,0 +1,123 @@
+from math import log10
+from math import sqrt
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pa
+import pandas as pd
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn.cross_validation import KFold
+from sklearn.feature_selection import f_regression
+from sklearn.preprocessing import PolynomialFeatures
+
+from preprocess2 import preprocess_2
+
+
+def linear_regression(data, target):
+    lr = linear_model.LinearRegression(normalize=True)
+    kf = KFold(len(target), n_folds=10, shuffle=True, random_state=None)
+    RMSE_LINEAR = []
+    for train_index, test_index in kf:
+        data_train, data_test = data[train_index], data[test_index]
+        target_train, target_test = target[train_index], target[test_index]
+        lr.fit(data_train, target_train)
+        rmse_linear = sqrt(np.mean((lr.predict(data_test) - target_test) ** 2))
+        RMSE_LINEAR.append(rmse_linear)
+
+    # scores = cross_validation.cross_val_score(rfr,data_test, target_test.ravel, cv=10)
+    # print np.mean(scores)
+
+    F, pval = f_regression(data_test, lr.predict(data_test))
+    print(pval)
+
+    test_times = np.arange(1, 11)
+    plt.figure()
+    plt.plot(test_times, RMSE_LINEAR, label="RMSE in linear regression with 10-fold cv")
+    #    plt.ylim(0.0, 0.12)
+    # plt.title("RMSE comparison between linear regression and random forest regression")
+    plt.xlabel("cross validation times")
+    plt.ylabel("RMSE")
+    plt.legend()
+
+    predicted = lr.predict(data)
+    index = np.arange(1, len(predicted) + 1)
+
+    plt.figure()
+    plt.scatter(index, target, s=15, color='red', label="Actual")
+    plt.scatter(index, predicted, s=15, color='green', label="Fitted")
+    plt.xlabel('Index')
+    plt.ylabel('MEDV')
+    plt.legend()
+
+    plt.figure()
+    plt.scatter(predicted, predicted - target, label="residual VS fitted values")
+    plt.xlabel("fitted values")
+    plt.ylabel("residual")
+    plt.legend()
+    #    plt.ylim(-0.8,0.4)
+    plt.show()
+    return RMSE_LINEAR
+
+
+def polynomial_regression():
+    boston = datasets.load_boston()
+
+    # linear-regression
+    lr = linear_model.LinearRegression()
+
+    test_times = range(0, 10)
+
+    plt.figure(1)
+    # divide the data into two sets
+    Train_set_x = pd.DataFrame(boston.data, index=None)
+    Train_set_y = pd.DataFrame(boston.target, index=None)
+    # split the data into 10 folds
+    f10 = KFold(len(Train_set_x), n_folds=10, shuffle=True, random_state=None)
+    # result1 linear,result2 poly-2,result3 poly-3,result4 poly-4,result5 poly-5,result6 poly-6,result7 poly-7
+
+    for i in range(1, 8):
+        results = []
+        for train_index, test_index in f10:
+            x_train, x_test = Train_set_x.iloc[train_index], Train_set_x.iloc[test_index]
+            y_train, y_test = Train_set_y.iloc[train_index], Train_set_y.iloc[test_index]
+
+            poly = PolynomialFeatures(degree=i)
+            x_train_2 = poly.fit_transform(x_train)
+            x_test_2 = poly.fit_transform(x_test)
+            lr.fit(x_train_2, y_train)
+            error = log10(sqrt(np.mean((lr.predict(x_test_2) - y_test) ** 2)))
+            results.append(error)
+        print results
+        plt.plot(test_times, results, label=('RMSE of Ploynomial Regression--Degree' + str(i)))
+    plt.legend(fontsize=6)
+    plt.title('Comparsion of Linear Regression and Polynomial Regression--Boston')
+    plt.xlabel('Test Times')
+    plt.ylabel('RMSE')
+    plt.ylim(0.0, 7)
+    plt.show()
+    plt.savefig('problem4-2')
+
+
+    # predicted = cross_val_predict(lr, Train_set_x, Train_set_y, cv=10)
+    #
+    # fig, ax = plt.subplots()
+    # ax.scatter(Train_set_y, predicted)
+    # ax.plot([Train_set_y.min(), Train_set_y.max()], [Train_set_y.min(), Train_set_y.max()], 'k--', lw=4)
+    # ax.set_xlabel('Measured')
+    # ax.set_ylabel('Predicted')
+    # plt.show()
+
+def main():
+    data = pa.read_csv("../Data/housing_data.csv", header=None).values[:, :]
+    binaryData = preprocess_2(data)
+    target = data[:, 13]
+    #rmse_linear = linear_regression(binaryData, target)
+    #rmse_l1CV=lassoCV_regression(binaryData,target)
+    #print("Mean RMSE of linear regression=" + str(np.mean(rmse_linear)))
+    # print(np.mean(rmse_l1CV))
+    polynomial_regression()
+
+
+if __name__ == "__main__":
+    main()
