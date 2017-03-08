a0a40f2f6b62653ef319790ae3924391c3134738
Guanqun Mao <guanqunm@gmail.com>
2017-03-07 22:22:30 -0800

    updated 3 4

--- hw4/src/part3.py
+++ hw4/src/part3.py
@@ -9,9 +9,95 @@ from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+##### reduce feature dimension: LSI/LSA
+# Vectorizer results are normalized, which makes KMeans behave as
+# spherical k-means for better results. Since LSA/SVD results are
+# not normalized, we have to redo the normalization.
+def LSA(X):
+    # n_components = [2, 5, 10, 50, 100, 200, 500, 1000]
+    n_components = [100, 300, 500]
+    homogeneity = []
+    completeness = []
+    vmeasure = []
+    ari = []
+    for dimensionality in np.array(n_components):
+        print("Desired dimensionality: %d" % dimensionality)
+        svd = TruncatedSVD(dimensionality)
+        normalizer = Normalizer(copy=False)
+        lsa = make_pipeline(svd, normalizer)
+        Y = lsa.fit_transform(X)
+
+        explained_variance = svd.explained_variance_ratio_.sum()
+        print("Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
+
+        km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+
+        # print("Clustering sparse data with %s" % km)
+        km.fit(Y)
+
+        homogeneity.append(metrics.homogeneity_score(labels, km.labels_))
+        completeness.append(metrics.completeness_score(labels, km.labels_))
+        vmeasure.append(metrics.v_measure_score(labels, km.labels_))
+        ari.append(metrics.adjusted_rand_score(labels, km.labels_))
+
+        print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
+        print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
+        print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
+        print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(labels, km.labels_))
+        # print("Adjusted Mutual Info: %.3f" % metrics.adjusted_mutual_info_score(labels, km.labels_)) === homogeneity
+        print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(Y, km.labels_, sample_size=1000))
+        print()
+
+    l1, = plt.plot(n_components, homogeneity, label='homogeneity')
+    l2, = plt.plot(n_components, completeness, label='completeness')
+    l3, = plt.plot(n_components, vmeasure, label='vmeasure')
+    l4, = plt.plot(n_components, ari, label='ari')
+    plt.legend(handles=[l1, l2, l3, l4])
+    plt.show()
+    print("------------------------------------------------------------------")
+
+
+##### reduce feature dimension: NMF
+def myNMF(X):
+    n_components = range(2, 21, 1)
+    # n_components = [500, 600, 700]
+    homogeneity = []
+    completeness = []
+    vmeasure = []
+    ari = []
+    for dimensionality in np.array(n_components):
+        print("Desired dimensionality: %d" % dimensionality)
+        nmf = NMF(n_components=dimensionality, init='nndsvd', random_state=0, alpha=.1, l1_ratio=0)
+        Y = nmf.fit_transform(X)
+        km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+
+        km.fit(Y)
+
+        homogeneity.append(metrics.homogeneity_score(labels, km.labels_))
+        completeness.append(metrics.completeness_score(labels, km.labels_))
+        vmeasure.append(metrics.v_measure_score(labels, km.labels_))
+        ari.append(metrics.adjusted_rand_score(labels, km.labels_))
+
+        print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
+        print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
+        print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
+        print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(labels, km.labels_))
+        # print("Adjusted Mutual Info: %.3f" % metrics.adjusted_mutual_info_score(labels, km.labels_))
+        print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(Y, km.labels_, sample_size=1000))
+        print()
+
+    l1, = plt.plot(n_components, homogeneity, label='homogeneity')
+    l2, = plt.plot(n_components, completeness, label='completeness')
+    l3, = plt.plot(n_components, vmeasure, label='vmeasure')
+    l4, = plt.plot(n_components, ari, label='ari')
+    plt.legend(handles=[l1, l2, l3, l4])
+    plt.show()
+    print("------------------------------------------------------------------")
+
+
 categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
               'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
-
 dataset = f20(subset='all', categories=categories, shuffle=True, random_state=42,
               remove=('headers', 'footers', 'quotes'))
 
@@ -31,73 +117,8 @@ X = vectorizer.fit_transform(dataset.data)
 samples, features = X.shape
 print("Number of samples: %d, number of features: %d" % (samples, features))
 print()
-
-##### reduce feature dimension: LSI/LSA
-# Vectorizer results are normalized, which makes KMeans behave as
-# spherical k-means for better results. Since LSA/SVD results are
-# not normalized, we have to redo the normalization.
-n_components = [2, 5, 10, 50, 100]
-homogeneity = []
-completeness = []
-vmeasure = []
-ari = []
-ami = []
-for dimensionality in np.array(n_components):
-    print("Desired dimensionality: %d" % dimensionality)
-    svd = TruncatedSVD(dimensionality)
-    normalizer = Normalizer(copy=False)
-    lsa = make_pipeline(svd, normalizer)
-    Y = lsa.fit_transform(X)
-
-    explained_variance = svd.explained_variance_ratio_.sum()
-    print("Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
-
-    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-
-    # print("Clustering sparse data with %s" % km)
-    km.fit(Y)
-
-    homogeneity.append(metrics.homogeneity_score(labels, km.labels_))
-    completeness.append(metrics.completeness_score(labels, km.labels_))
-    vmeasure.append(metrics.v_measure_score(labels, km.labels_))
-    ari.append(metrics.adjusted_rand_score(labels, km.labels_))
-    ami.append(metrics.adjusted_mutual_info_score(labels, km.labels_))
-
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
-    print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(labels, km.labels_))
-    print("Adjusted Mutual Info: %.3f" % metrics.adjusted_mutual_info_score(labels, km.labels_))
-    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(Y, km.labels_, sample_size=1000))
-    print()
-
-plt.plot(n_components, homogeneity)
-plt.plot(n_components, completeness)
-plt.plot(n_components, vmeasure)
-plt.plot(n_components, ari)
-plt.plot(n_components, ami)
-plt.show()
-
-print("------------------------------------------------------------------")
-
-'''
-##### reduce feature dimension: NMF
-for dimensionality in np.array(n_components):
-    print("Desired dimensionality: %d" % dimensionality)
-    nmf = NMF(n_components=dimensionality, random_state=0, alpha=.1, l1_ratio=.5)
-    Y = nmf.fit_transform(X)
-    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-
-    km.fit(Y)
-
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
-    print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(labels, km.labels_))
-    print("Adjusted Mutual Info: %.3f" % metrics.adjusted_mutual_info_score(labels, km.labels_))
-    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(Y, km.labels_, sample_size=1000))
-    print()
-'''
+# LSA(X)
+myNMF(X)
 
 '''
 print("Top terms per cluster:")
--- hw4/src/part4.py
+++ hw4/src/part4.py
@@ -1,88 +1,78 @@
 from __future__ import print_function
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction import text
-from sklearn import metrics
-from nltk import SnowballStemmer
-from nltk.tokenize import RegexpTokenizer
-from sklearn.cluster import KMeans
-import numpy as np
-from sklearn.preprocessing import Normalizer
+from sklearn.datasets import fetch_20newsgroups as f20
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD, NMF
 from sklearn.pipeline import make_pipeline
-from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans
+from sklearn import metrics
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
 
-# Get stop words
-def get_stop_words():
-    stop_words = [text.ENGLISH_STOP_WORDS]
-    with open('NLTK_StopWords.txt', 'r') as file:
-        lists = file.readlines()
+categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
+              'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
+dataset = f20(subset='all', categories=categories, shuffle=True, random_state=42,
+              remove=('headers', 'footers', 'quotes'))
 
-    for i in range(len(lists)):
-        lists[i] = lists[i].rstrip()  # remove trailing spaces
+'''
+print("%d documents" % len(dataset.data))
+print("%d categories" % len(dataset.target_names))
+'''
 
-    NLTK_stop_words = lists
+labels = dataset.target
+true_k = 2
 
-    return stop_words, NLTK_stop_words
+##### reduce feature dimension: min_df=2
+print("Reduce feature dimension by setting min_df = 2")
+vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words='english')
+X = vectorizer.fit_transform(dataset.data)
+samples, features = X.shape
+print("Number of samples: %d, number of features: %d" % (samples, features))
+print()
 
-# Proprocess data
-def preprocess(sentence, stop_words, NLTK_stop_words):
-    sentence = sentence.lower()
-    tokenizer = RegexpTokenizer(r'\w+')
-    stemmer = SnowballStemmer("english")
+# set dimensionality to 7
+dimensionality = 7
+nmf = NMF(n_components=dimensionality, init='nndsvd', random_state=0, alpha=.1, l1_ratio=0)
+Y = nmf.fit_transform(X)
 
-    tokens = tokenizer.tokenize(sentence)
-    filter1 = [w for w in tokens if not w in NLTK_stop_words and w not in stop_words and len(w) is not 1]
-    filter2 = [stemmer.stem(plural) for plural in filter1]
-    filter3 = [i for i in filter2 if not i.isdigit()]
-    return " ".join(filter3)
+data = scale(Y)
 
-# Calculate TFIDF
-def TFIDF(categories, train_test, stop_words, NLTK_stop_words):
-    # Get the data
-    twenty_data = fetch_20newsgroups(subset=train_test,categories=categories, remove=('headers','footers','quotes'))
-    # Save size of the data
-    size, = twenty_data.filenames.shape
-    # Preprocess all documents
-    for item in range(0, size):
-        sentence = twenty_data.data[item]
-        twenty_data.data[item] = preprocess(sentence, stop_words, NLTK_stop_words)
+reduced_data = PCA(n_components=2).fit_transform(data)
+kmeans = KMeans(init='k-means++', n_clusters=2, n_init=10)
+kmeans.fit(reduced_data)
 
-    # Transfer processed data to a TDM
-    count_vectorizer = CountVectorizer()
-    X_train_number = count_vectorizer.fit_transform(twenty_data.data)
+# Step size of the mesh. Decrease to increase the quality of the VQ.
+h = .02  # point in the mesh [x_min, x_max]x[y_min, y_max].
 
-    # Calculate TFIDF for every term in the document
-    tf_transformer = TfidfTransformer(use_idf=True).fit(X_train_number)
-    X_train_tfidf = tf_transformer.transform(X_train_number)
-    docs, terms = X_train_tfidf.shape
-    print(100 * '_')
-    print("TFIDF Matrix Created")
-    print("Final number of terms: ", terms)
-    print("Final number of documents: ", docs)
-    print(100 * '_')
-    return twenty_data, X_train_tfidf, X_train_number, count_vectorizer
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-# Load data
-def load_data():
-    categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
-                  'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
+# Obtain labels for each point in mesh. Use last trained model.
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
 
-    stop_words, NLTK_stop_words = get_stop_words()
-    data, X_tfidf, X_counts, count_vectorizer = TFIDF(categories, 'all', stop_words, NLTK_stop_words)
-    return data, X_tfidf
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
 
-def d():
-    data, X_tfidf = load_data()
-
-    number_of_components = 50
-    number_of_iterations = 200
-    np.random.seed(304145309)
-
-    svd = TruncatedSVD(n_components=number_of_components, n_iter=number_of_iterations)
-    normalizer_pca = Normalizer(copy=False)
-    lsa_pca = make_pipeline(svd, normalizer_pca)
-    X_tfidf_pca = lsa_pca.fit_transform(X_tfidf)
-    print(X_tfidf_pca.shape)
-
-d()
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+# Plot the centroids as a white O
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.title('K-means clustering on the 20newsgroup \n'
+          'Centroids are white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
