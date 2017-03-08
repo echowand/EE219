384d7a26c44f3633000b73aa5f1e69c311482341
JLiu1994 <jianing.liu@vanderbilt.edu>
2017-03-07 22:24:53 -0800

    Updated part 6.

--- hw4/src/part6.py
+++ hw4/src/part6.py
@@ -15,9 +15,23 @@ vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words='english')
 dataset = f20(subset='all', categories=categories, shuffle=True, random_state=42,
               remove=('headers', 'footers', 'quotes'))
 labels = dataset.target
+
+# map labels
+dict = {1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0,
+        7 : 1, 8 : 1, 9 : 1, 10 : 1,
+        11 : 2, 12 : 2, 13 : 2, 14 : 2,
+        6 : 3,
+        16 : 4, 17 : 4, 18 : 4,
+        0 : 5, 15 : 5, 19 : 5}
+
+i = 0
+for label in labels:
+    labels[i] = dict[labels[i]]
+    i = i+1
+
 X = vectorizer.fit_transform(dataset.data)
 
-km = KMeans(n_clusters=20, max_iter = 1000, random_state=42).fit(X)
+km = KMeans(n_clusters=6, max_iter = 1000, random_state=42).fit(X)
 
 print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
 print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
@@ -25,10 +39,9 @@ print("Adjusted Rand-Index: %.3f"
       % metrics.adjusted_rand_score(labels, km.labels_))
 print("Adjusted_Mutual_Info_Score: %.3f"
       % metrics.adjusted_mutual_info_score(labels, km.labels_))
-print ()
+print()
 
 # # SVD
-#
 # n_components = [2, 5, 10, 50, 100]
 #
 # for dimensionality in np.array(n_components):
@@ -57,7 +70,7 @@ for dimensionality in np.array(n_components):
 
     nmf = NMF(n_components=dimensionality, init='nndsvd', random_state=0, alpha=.1, l1_ratio=0)
     dataset_NMF = nmf.fit_transform(X)
-    kmNMF = KMeans(n_clusters=20, max_iter=1000, random_state=42).fit(dataset_NMF)
+    kmNMF = KMeans(n_clusters=6, max_iter=1000, random_state=42).fit(dataset_NMF)
 
     print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, kmNMF.labels_))
     print("Completeness: %0.3f" % metrics.completeness_score(labels, kmNMF.labels_))
