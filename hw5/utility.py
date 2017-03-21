7848968ef3fa7f997ad138e6bdc38c225e2c7e0f
JLiu1994 <jianing.liu@vanderbilt.edu>
2017-03-21 13:18:43 -0700

    Updated utility.py

--- hw5/utility.py
+++ hw5/utility.py
@@ -7,7 +7,6 @@ import logging as logger
 import statsmodels.api as sm
 from datetime import datetime
 from collections import Counter
-import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from sklearn.cross_validation import KFold
 
@@ -17,41 +16,16 @@ extra_features = ['Number of Tweets', 'Number of Retweets', 'Number of Followers
                   'Impression Count', 'Favourite Count', 'Ranking Score', 'Hour of Day', 'Number of Users tweeting',
                   'Number of Long Tweets']
 
+stop_words = stopwords.words("english")
+punctuation = list(string.punctuation)
+remove_words = stop_words + punctuation
 
-def extra_feature_dict():
-    """
-    :return: create feature dictionary for features calculation
-    """
-    features = dict.fromkeys(extra_features, 0)
-    features['Number of Users tweeting'] = []
-
-    return features
-
-
-def calculate_statistic(features, tweet_data):
-    """
-    :return: return calculated features of the tweet data
-    """
-    features['Number of Tweets'] += 1
-    features['Number of Retweets'] += tweet_data.get('metrics').get('citations').get('total')
-    follower_count = tweet_data.get('author').get('followers')
-    features['Number of Followers'] += follower_count
-    features['Impression Count'] += tweet_data.get('metrics').get('impressions')
-    if follower_count > features['Max Number of Followers']:
-        features['Max Number of Followers'] = follower_count
-    features['Ranking Score'] += tweet_data.get('metrics').get('ranking_score')
-    features['Hour of Day'] = int(datetime.fromtimestamp(tweet_data.get('firstpost_date')).strftime("%H"))
-    features['Favourite Count'] += tweet_data.get('tweet').get('favorite_count')
-    features['Number of Users tweeting'].append(tweet_data.get('tweet').get('user').get('id'))
-    features['Number of Long Tweets'] += 1 if len(tweet_data.get('title')) > 100 else 0
-
-    return features
-
+top_word_count = Counter()
+top_bigrams_count = Counter()
+top_hash_tags = Counter()
+all_tweets = []
 
 def get_features(features):
-    """
-    :return: get feature values in specified order of extra_features
-    """
     extract = []
     for i in extra_features:
         extract.append(features[i])
@@ -60,60 +34,22 @@ def get_features(features):
 
     return extract
 
-
-def reset_features_dict():
-    """
-    :return: reset the features dict for new window calculation
-    """
+def extra_feature_dict():
     features = dict.fromkeys(extra_features, 0)
     features['Number of Users tweeting'] = []
 
     return features
 
+def reset_features_dict():
+    features = dict.fromkeys(extra_features, 0)
+    features['Number of Users tweeting'] = []
 
-def perform_classification(X, Y):
-    """
-    :return: 10 fold cross validation of X and Y
-    """
-
-    average_error = []
-    kf = KFold(len(X), n_folds=10, shuffle=False, random_state=None)
-
-    for train_index, test_index in kf:
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = Y[train_index], Y[test_index]
-
-        y_train = collections.deque(y_train)
-        y_train.rotate(-1)
-        y_train = list(y_train)
-        X_train = list(X_train)
-
-        result = sm.OLS(y_train, X_train).fit()
-        test_prediction = result.predict(X_test)
-        average_error.append(np.mean(abs(test_prediction - y_test)))
-
-    logger.info("10 Fold Error : {}".format(average_error))
-    logger.info("Average error : {}".format(np.mean(average_error)))
-
-
-stopWords = stopwords.words("english")
-punctuation = list(string.punctuation)
-remove_words = stopWords + punctuation
-
-top_word_count = Counter()
-top_bigrams_counter = Counter()
-top_hash_tags = Counter()
-all_tweets = []
-
+    return features
 
 def preprocess_data(hourly_tweet_texts):
-    """
-    :return: preprocessed data of tweets from one hour window
-    """
-    # count top words
     word_count = Counter()
     other_hash_tags = Counter()
-    bigrams_counter = Counter()
+    bigrams_count = Counter()
     word_list = []
     terms_only = Counter()
 
@@ -137,11 +73,46 @@ def preprocess_data(hourly_tweet_texts):
         terms_only.update(temp)
         top_word_count.update(temp)
 
-        bigrams_counter.update(bigrams(temp))
-        top_bigrams_counter.update(bigrams(temp))
+        bigrams_count.update(bigrams(temp))
+        top_bigrams_count.update(bigrams(temp))
         top_hash_tags.update([term.lower() for term in tweet_text.split()
                               if term.startswith('#')])
         word_list.append(terms_only)
 
-    return word_list, other_hash_tags, terms_only, bigrams_counter
+    return word_list, other_hash_tags, terms_only, bigrams_count
 
+def perform_classification(X, Y):
+    average_error = []
+    kf = KFold(len(X), n_folds=10, shuffle=False, random_state=None)
+
+    for train_index, test_index in kf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+
+        y_train = collections.deque(y_train)
+        y_train.rotate(-1)
+        y_train = list(y_train)
+        X_train = list(X_train)
+
+        result = sm.OLS(y_train, X_train).fit()
+        test_prediction = result.predict(X_test)
+        average_error.append(np.mean(abs(test_prediction - y_test)))
+
+    logger.info("10 Fold Error : {}".format(average_error))
+    logger.info("Average error : {}".format(np.mean(average_error)))
+
+def calculate_statistic(features, tweet_data):
+    features['Number of Tweets'] += 1
+    features['Number of Retweets'] += tweet_data.get('metrics').get('citations').get('total')
+    follower_count = tweet_data.get('author').get('followers')
+    features['Number of Followers'] += follower_count
+    features['Impression Count'] += tweet_data.get('metrics').get('impressions')
+    if follower_count > features['Max Number of Followers']:
+        features['Max Number of Followers'] = follower_count
+    features['Ranking Score'] += tweet_data.get('metrics').get('ranking_score')
+    features['Hour of Day'] = int(datetime.fromtimestamp(tweet_data.get('firstpost_date')).strftime("%H"))
+    features['Favourite Count'] += tweet_data.get('tweet').get('favorite_count')
+    features['Number of Users tweeting'].append(tweet_data.get('tweet').get('user').get('id'))
+    features['Number of Long Tweets'] += 1 if len(tweet_data.get('title')) > 100 else 0
+
+    return features
